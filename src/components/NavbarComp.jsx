d4b805f0c820e2c4038709d7dd95da727447398d
M Zulkifli <98865844+kiflycules@users.noreply.github.com>
2022-04-01 17:59:30 +0800

    initial commit

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,19 +1,26 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 function NavbarComp() {
+  const navigate = useNavigate();
+
+  const movePage = (path) => {
+    navigate(path);
+  };
   return (
     <Navbar collapseOnSelect expand="lg">
-        <Navbar.Brand href="#home">Logo Brand</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link>sub brand 1</Nav.Link>
-            <Nav.Link>sub brand 2</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link>Login</Nav.Link>
-            <Nav.Link>Register</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar.Brand onClick={() => movePage("/")}>FooS.</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link onClick={() => movePage("/")}>Beranda</Nav.Link>
+          <Nav.Link>Menu</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link onClick={() => movePage("/login")}>Masuk</Nav.Link>
+          <Nav.Link onClick={() => movePage("/register")}>Daftar</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
