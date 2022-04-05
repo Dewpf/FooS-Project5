e4b233df5a9912345d94988c806f7aef91a6882c
M Zulkifli <kiflyfcb7@gmail.com>
2022-04-05 22:41:08 +0800

    add product

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,49 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Row, Col} from "react-bootstrap"
-import { API_URL } from '../Data_URL'
-import axios from 'axios'
 
-export default class MenuPage extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         menus: [],
-      }
-    }
-
-    componentDidMount(){
-        axios.get(API_URL+"products")
-            .then(res => {
-                // console.log("response", res)
-                const menus = res.data;
-                this.setState({ menus });
-            })
-            .catch(err => {
-                console.log("error maszeehhh", err)
-            })
-    }
-    
-    render() {
-        // console.log(this.state.menus)
-        return (
-            <div  style={{
-                marginTop: "2px",
-                paddingTop: "6px",
-                cursor: "pointer"
-            }}>
-                <Container>
-                    <Row>
-                        <Col><strong>Makanan</strong></Col>
-                        <Col><strong>Minuman</strong></Col>
-                        <Col><strong>Cemilan</strong></Col>
-                        <Col><strong>Oleh-Oleh</strong></Col>
-                    </Row>
-                    {menus && menus.map((menu) =>(
-                        <h2>{menu.id}</h2>
-                    ))}
-                </Container>
-            </div>
-        )
-    }
+function MenuPage (){
+    return (
+        <div  style={{
+            marginTop: "2px",
+            paddingTop: "6px",
+            cursor: "pointer"
+        }}>
+            <Container>
+                <Row>
+                    <Col><strong>Makanan</strong></Col>
+                    <Col><strong>Minuman</strong></Col>
+                    <Col><strong>Cemilan</strong></Col>
+                    <Col><strong>Oleh-Oleh</strong></Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
+
+export default MenuPage
